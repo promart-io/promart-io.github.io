6231cf93424660a9e7ec082847df557051b5ad7e
Yordan Pavlov <ThuF@users.noreply.github.com>
2018-08-28 15:27:50 +0300

    Create recent.js

---
+++ marketplace/js/controllers/marketplace/apps/recent.js
@@ -0,0 +1,31 @@
+angular.module('marketplace')
+.controller('RecentAppsController', function ($scope, $http) {
+
+	$scope.activeTab = 'recentApps';
+
+	var api = 'https://ide.ingress.dev.promart.shoot.canary.k8s-hana.ondemand.com/services/v3/js/marketplace/api/apps.js';
+
+	function loadApps() {
+		$http.get(api + '/recent')
+		.success(function(data) {
+			$scope.recentApps = data;
+		});
+	}
+	loadApps();
+
+	function loadTopPaidApps() {
+		$http.get(api + '/top/paid')
+		.success(function(data) {
+			$scope.topPaidApps = data;
+		});
+	}
+	loadTopPaidApps();
+
+	function loadTopFreeApps() {
+		$http.get(api + '/top/free')
+		.success(function(data) {
+			$scope.topFreeApps = data;
+		});
+	}
+	loadTopFreeApps();
+});
