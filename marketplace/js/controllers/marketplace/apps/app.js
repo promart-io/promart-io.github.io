39a8e24799283c19635d730710e501d1a96de57d
Yordan Pavlov <ThuF@users.noreply.github.com>
2018-08-29 14:35:52 +0300

    Update app.js

--- marketplace/js/controllers/marketplace/apps/app.js
+++ marketplace/js/controllers/marketplace/apps/app.js
@@ -1,6 +1,15 @@
 angular.module('marketplace')
 .controller('AppController', function ($scope, $http, $routeParams) {
 
-	$scope.application = $routeParams.appId;
+
+	var api = 'http://diving-shop.ingress.dev.promart.shoot.canary.k8s-hana.ondemand.com/services/v3/js/marketplace/api/apps.js';
+
+	function getApp() {
+		$http.get(api + '/' + $routeParams.appId)
+		.success(function(data) {
+			$scope.application = data;
+		});
+	}
+	getApp();
 
 });
