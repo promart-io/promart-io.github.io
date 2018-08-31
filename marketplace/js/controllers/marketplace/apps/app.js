147e29ec63355548a7a70d4596d4fa4bacc4235e
Yordan Pavlov <ThuF@users.noreply.github.com>
2018-08-31 10:57:34 +0300

    Update app.js

--- marketplace/js/controllers/marketplace/apps/app.js
+++ marketplace/js/controllers/marketplace/apps/app.js
@@ -2,7 +2,7 @@ angular.module('marketplace')
 .controller('AppController', function ($scope, $http, $routeParams) {
 
 
-	var api = 'https://store.ingress.dev.promart.shoot.canary.k8s-hana.ondemand.com/services/v3/js/marketplace/api/apps.js';
+	var api = 'https://store.ingress.dev.promart.shoot.canary.k8s-hana.ondemand.com/services/v3/js/marketplace/api/Products.js';
 
 	function getApp() {
 		$http.get(api + '/' + $routeParams.appId)
